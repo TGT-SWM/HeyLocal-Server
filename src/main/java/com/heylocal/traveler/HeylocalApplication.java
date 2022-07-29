cfede2a94c0308e91593059789691c4b4ff3fc4b
TaegyunWoo <dnxprbs@gmail.com>
2022-07-29 18:26:04 +0900

    Init: Package 이름 수정
    도메인에 맞춰 수정함

--- src/main/java/com/heylocal/traveler/HeylocalApplication.java
+++ src/main/java/com/heylocal/traveler/HeylocalApplication.java
@@ -1,4 +1,4 @@
-package com.heylocal.api;
+package com.heylocal.traveler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/heylocal/traveler/HeylocalApplicationTests.java
+++ src/test/java/com/heylocal/traveler/HeylocalApplicationTests.java
@@ -1,4 +1,4 @@
-package com.heylocal.api;
+package com.heylocal.traveler;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
