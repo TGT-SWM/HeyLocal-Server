009e3157f50f8eb3f2b62589c7b686abafb15a9d
TaegyunWoo <dnxprbs@gmail.com>
2022-08-06 00:56:44 +0900

    Feat: 개발용 Auto DDL 설정 변경
    Create-Drop 으로 설정시, dml 스크립트를 읽는데 이슈가 있어 Create 로 수정함.

--- src/main/java/com/heylocal/traveler/domain/user/User.java
+++ src/main/java/com/heylocal/traveler/domain/user/User.java
@@ -1,14 +1,38 @@
 package com.heylocal.traveler.domain.user;
 
+import com.heylocal.traveler.domain.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
 /**
  * 사용자(여행자, 매니저, 서비스관리자) 부모 클래스
  * TODO https://swm13-tgt.atlassian.net/browse/S3T-298 반영해야함
  */
-public class User {
+
+@Entity
+@Table(name = "USER")
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "DTYPE")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class User extends BaseTimeEntity {
+  @Id @GeneratedValue
   private long id;
+
+  @Column(length = 20, nullable = false, unique = true)
   private String accountId;
+
+  @Column(length = 50, nullable = false)
   private String password;
-  private String nickname;
+
+  @Column(length = 13, nullable = false, unique = true)
   private String phoneNumber;
-  private UserType type;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private UserType userType;
 }
