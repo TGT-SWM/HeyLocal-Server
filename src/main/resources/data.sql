13e3d99d928f9d212d33060b6e1c8b8df4bd625d
TaegyunWoo <dnxprbs@gmail.com>
2022-08-07 01:23:52 +0900

    Feat: 테스트용 DB와 연동 가능한 properties 파일 추가 및 설정
    - local 용 properties 파일과 stage 서버용 properties 파일을 구분하여 작성함.

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-/* 매니저 INSERT */
-insert into "user"("id", "account_id", "password", "phone_number", "user_type", "dtype") values (0, 'admin', 'admin123', '010-1234-1234', 'SERVICE_MANAGER', 'SERVICE_MANAGER');
-insert into "service_manager"("id", "nickname") values (0, 'admin');
