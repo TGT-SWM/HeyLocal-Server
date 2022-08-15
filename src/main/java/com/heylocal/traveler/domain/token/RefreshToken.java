2513a560f0d16e0fa3cc15e0cbbabc12a89a4eaa
TaegyunWoo <dnxprbs@gmail.com>
2022-08-15 17:44:05 +0900

    S3T-100-Feat: Token 엔티티 추가

---
+++ src/main/java/com/heylocal/traveler/domain/token/RefreshToken.java
@@ -0,0 +1,28 @@
+package com.heylocal.traveler.domain.token;
+
+import com.heylocal.traveler.domain.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "REFRESH_TOKEN")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@SuperBuilder
+public class RefreshToken extends BaseTimeEntity {
+  @Id
+  @GeneratedValue
+  private long id;
+  private String tokenValue;
+  private LocalDateTime expiredDateTime;
+
+  @OneToOne
+  @JoinColumn(nullable = false)
+  private AccessToken accessToken;
+}
