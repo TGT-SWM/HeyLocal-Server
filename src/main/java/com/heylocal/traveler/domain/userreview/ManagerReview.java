95fe3c126e9e9cf958a6ec786d607fd322ccebc2
Woody <gintooooonic@gmail.com>
2022-08-22 06:48:23 +0900

    S3T-17-Fix: ManagerReview의 travel을 NOT NULL하게 수정
    ManagerReview에서 travel이 항상 존재해야 하므로
    nullable 제약 조건을 false로 수정합니다.

--- src/main/java/com/heylocal/traveler/domain/userreview/ManagerReview.java
+++ src/main/java/com/heylocal/traveler/domain/userreview/ManagerReview.java
@@ -22,7 +22,7 @@ import javax.persistence.*;
 @SuperBuilder
 public class ManagerReview extends UserReview {
   @OneToOne
-  @JoinColumn(nullable = true)
+  @JoinColumn(nullable = false)
   private Travel travel;
 
   @Enumerated(EnumType.STRING)
