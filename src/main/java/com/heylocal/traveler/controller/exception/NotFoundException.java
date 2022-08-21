4f128c28c4cf9c23f748ae8f02ab63cc92b9c590
Woody <gintooooonic@gmail.com>
2022-08-22 07:20:44 +0900

    S3T-17-Fix: NotFoundException 주석 추가

--- src/main/java/com/heylocal/traveler/controller/exception/NotFoundException.java
+++ src/main/java/com/heylocal/traveler/controller/exception/NotFoundException.java
@@ -2,6 +2,11 @@ package com.heylocal.traveler.controller.exception;
 
 import lombok.NoArgsConstructor;
 
+/**
+ * <pre>
+ * 404 Not Found 관련 예외
+ * </pre>
+ */
 @NoArgsConstructor
 public class NotFoundException extends Exception {
 	public NotFoundException(String message) {
