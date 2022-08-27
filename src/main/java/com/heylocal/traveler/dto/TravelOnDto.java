4c2c140fd66e221c1b7507267a2cbe82c8b3860e
Woody <gintooooonic@gmail.com>
2022-08-28 05:55:23 +0900

    S3T-378-Feat: TravelOns API 추가
    여행 On API를 추가하였습니다.

--- src/main/java/com/heylocal/traveler/dto/TravelOnDto.java
+++ src/main/java/com/heylocal/traveler/dto/TravelOnDto.java
@@ -9,8 +9,32 @@ public class TravelOnDto {
 	@NoArgsConstructor
 	@AllArgsConstructor
 	@Builder
+	@Schema(description = "여행 On 생성을 위한 요청 DTO")
+	public static class TravelOnRequest {
+		long id;
+	}
+
+	@Getter
+	@Setter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Builder
+	@Schema(description = "여행 On 응답 DTO")
+	public static class TravelOnResponse {
+		long id;
+	}
+
+	@Getter
+	@Setter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Builder
 	@Schema(description = "여행 On 목록에 띄우기 위한 간략한 여행 On 응답 DTO")
 	public static class TravelOnSimpleResponse {
 		long id;
+	}
+
+	public enum TravelOnSortType {
+		DATE, VIEWS, OPINIONS
 	}
 }
