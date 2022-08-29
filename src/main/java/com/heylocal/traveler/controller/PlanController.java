1c091d9d736445dcbccc7f8fc70801d5c05566cf
Woody <gintooooonic@gmail.com>
2022-08-29 14:48:48 +0900

    S3T-395-Feat: PlanController 생성
    PlansApi의 구현체인 PlanController를 생성합니다.

---
+++ src/main/java/com/heylocal/traveler/controller/PlanController.java
@@ -0,0 +1,70 @@
+package com.heylocal.traveler.controller;
+
+import com.heylocal.traveler.controller.api.PlansApi;
+import com.heylocal.traveler.dto.PlanDto.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Tag(name = "Plans")
+@RestController
+public class PlanController implements PlansApi {
+	/**
+	 * @return
+	 */
+	@Override
+	public PlanListResponse getPlans() {
+		return null;
+	}
+
+	/**
+	 * @param request
+	 * @return
+	 */
+	@Override
+	public ResponseEntity<Void> createPlan(PlanRequest request) {
+		return null;
+	}
+
+	/**
+	 * @param planId
+	 * @param request
+	 * @return
+	 */
+	@Override
+	public ResponseEntity<Void> updatePlan(long planId, PlanRequest request) {
+		return null;
+	}
+
+	/**
+	 * @param planId
+	 * @return
+	 */
+	@Override
+	public ResponseEntity<Void> deletePlan(long planId) {
+		return null;
+	}
+
+	/**
+	 * @param planId
+	 * @param day
+	 * @return
+	 */
+	@Override
+	public List<PlanPlacesResponse> getPlacesInPlan(long planId, int day) {
+		return null;
+	}
+
+	/**
+	 * @param planId
+	 * @param day
+	 * @param request
+	 * @return
+	 */
+	@Override
+	public ResponseEntity<Void> updatePlaceInPlan(long planId, int day, PlanPlacesRequest request) {
+		return null;
+	}
+}
