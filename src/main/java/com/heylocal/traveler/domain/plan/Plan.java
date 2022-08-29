402b60e336eda3b0b5b2a9775a47ff839c3ec11a
Woody <gintooooonic@gmail.com>
2022-08-29 19:29:10 +0900

    S3T-395-Feat: 마이 플랜 조회 기능 구현

--- src/main/java/com/heylocal/traveler/domain/plan/Plan.java
+++ src/main/java/com/heylocal/traveler/domain/plan/Plan.java
@@ -28,6 +28,7 @@ public class Plan extends BaseTimeEntity {
   @GeneratedValue
   private Long id;
 
+  // 별도로 저장하고 있는 것보다. 여행 날짜를 통해 유도하는 것은 어떨까?
   @Enumerated(EnumType.STRING)
   @Column(nullable = false)
   private PlanStatus status;
--- src/main/java/com/heylocal/traveler/service/PlanService.java
+++ src/main/java/com/heylocal/traveler/service/PlanService.java
@@ -6,7 +6,10 @@ import com.heylocal.traveler.repository.PlanRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -21,11 +24,38 @@ public class PlanService {
 	 * </pre>
 	 */
 	public PlanListResponse getPlans(long userId) {
-		// 작성한 스케줄 조회
+		// 작성한 플랜 조회
 		List<Plan> plans = planRepository.findAll(userId);
 
-		// DTO 변환 로직
+		// Plan -> PlanResponse 변환
+		List<PlanResponse> planResponses = plans.stream()
+				.map(PlanResponse::new)
+				.collect(Collectors.toList());
 
-		return null;
+		// 플랜을 날짜에 따라 분류
+		LocalDate today = LocalDate.now();
+		List<PlanResponse> past = new ArrayList<>();
+		List<PlanResponse> ongoing = new ArrayList<>();
+		List<PlanResponse> upcoming = new ArrayList<>();
+
+		planResponses.forEach(planResponse -> {
+			LocalDate startDate = planResponse.getStartDate();
+			LocalDate endDate = planResponse.getEndDate();
+
+			if (today.isBefore(startDate)) {
+				upcoming.add(planResponse);
+			} else if (today.isAfter(endDate)) {
+				past.add(planResponse);
+			} else {
+				ongoing.add(planResponse);
+			}
+		});
+
+		// PlanListResponse 생성 및 반환
+		return PlanListResponse.builder()
+				.past(past)
+				.ongoing(ongoing)
+				.upcoming(upcoming)
+				.build();
 	}
 }
