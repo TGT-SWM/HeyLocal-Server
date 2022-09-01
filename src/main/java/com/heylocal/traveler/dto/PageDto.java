6298ea989ca5b64b071a5c81ac3ecdd0c19ddf7b
TaegyunWoo <dnxprbs@gmail.com>
2022-09-01 14:19:50 +0900

    S3T-391-FEAT: PageRequest DTO 클래스에 Swagger API 설명 추가

--- src/main/java/com/heylocal/traveler/dto/PageDto.java
+++ src/main/java/com/heylocal/traveler/dto/PageDto.java
@@ -1,5 +1,6 @@
 package com.heylocal.traveler.dto;
 
+import io.swagger.annotations.ApiParam;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,9 +18,11 @@ public class PageDto {
 		 * 클라이언트가 마지막으로 받은 아이템의 ID이며,
 		 * null인 경우 아이템을 처음부터 조회합니다.
 		 */
+		@ApiParam("클라이언트가 마지막으로 받은 아이템의 ID <br/> null인 경우 아이템을 처음부터 조회")
 		Long lastId;
 
 		/** 응답으로 받고자 하는 아이템의 최대 개수 */
+		@ApiParam("응답으로 받고자 하는 아이템의 최대 개수")
 		int size;
 	}
 }
