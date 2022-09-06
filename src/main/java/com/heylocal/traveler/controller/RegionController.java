d71de0811f8f36322deb6241ab7f80a95c66ee2a
TaegyunWoo <dnxprbs@gmail.com>
2022-09-06 15:29:10 +0900

    S3T-428-Feat: Region 조회 API 추가

--- src/main/java/com/heylocal/traveler/controller/RegionController.java
+++ src/main/java/com/heylocal/traveler/controller/RegionController.java
@@ -2,19 +2,37 @@ package com.heylocal.traveler.controller;
 
 import com.heylocal.traveler.controller.api.RegionsApi;
 import com.heylocal.traveler.dto.RegionDto;
+import com.heylocal.traveler.exception.controller.NotFoundException;
+import com.heylocal.traveler.exception.service.BadArgumentException;
+import com.heylocal.traveler.service.RegionService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import static com.heylocal.traveler.dto.RegionDto.*;
+
 @Tag(name = "Regions")
 @RestController
+@RequiredArgsConstructor
 public class RegionController implements RegionsApi {
+	private final RegionService regionService;
+
 	/**
+	 * 지역 정보 조회 핸들러
 	 * @return
 	 */
 	@Override
-	public List<RegionDto.RegionResponse> getRegions() {
-		return null;
+	public List<RegionResponse> getRegions(String state) throws NotFoundException {
+		List<RegionResponse> response;
+
+		try {
+			response = regionService.inquiryRegions(state);
+		} catch (BadArgumentException e) {
+			throw new NotFoundException(e.getCode(), e.getDescription());
+		}
+
+		return response;
 	}
 }
---
+++ src/test/java/com/heylocal/traveler/service/RegionServiceTest.java
@@ -0,0 +1,7 @@
+package com.heylocal.traveler.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegionServiceTest {
+  // TODO
+}
