0b4615cce5eb8d5569224ffe3e292c779f71ca7a
TaegyunWoo <dnxprbs@gmail.com>
2022-09-08 13:01:47 +0900

    S3T-433-Feat: PlaceDto 요청 클래스 Swagger 스키마 설명 추가

--- src/main/java/com/heylocal/traveler/dto/PlaceDto.java
+++ src/main/java/com/heylocal/traveler/dto/PlaceDto.java
@@ -3,6 +3,9 @@ package com.heylocal.traveler.dto;
 import com.heylocal.traveler.domain.place.Place;
 import com.heylocal.traveler.domain.place.PlaceCategory;
 import com.heylocal.traveler.domain.plan.list.PlaceItem;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
@@ -17,22 +20,31 @@ public class PlaceDto {
 	@NoArgsConstructor
 	@AllArgsConstructor
 	@Builder
+	@ApiModel
 	@Schema(description = "장소 상세 정보 요청 DTO")
 	public static class PlaceRequest {
+		@ApiModelProperty(value = "카카오 장소 검색 API 에서 응답받은 장소 id", required = true)
 		@Positive
 		private long id;
+		@ApiModelProperty(value = "카카오 장소 검색 API 에서 응답받은 장소 카테고리", required = true)
 		@NotNull
 		private PlaceCategory category;
+		@ApiModelProperty(value = "장소 이름", required = true)
 		@NotEmpty
 		private String name;
+		@ApiModelProperty(value = "카카오 장소 검색 API 에서 응답받은 장소 도로명 주소", required = true)
 		@NotEmpty
 		private String roadAddress;
+		@ApiModelProperty(value = "카카오 장소 검색 API 에서 응답받은 장소 구주소", required = true)
 		@NotEmpty
 		private String address;
+		@ApiModelProperty(value = "카카오 장소 검색 API 에서 응답받은 위도값 (x)", required = true)
 		@Positive
 		private double lat;
+		@ApiModelProperty(value = "카카오 장소 검색 API 에서 응답받은 경도값 (y)", required = true)
 		@Positive
 		private double lng;
+		@ApiModelProperty(value = "카카오 장소 검색 API 에서 응답받은 카카오맵 link", required = true)
 		@NotEmpty
 		private String kakaoLink;
 	}
