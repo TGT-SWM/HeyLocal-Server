18aa3daac23c93333a2242c7cfad293258929740
TaegyunWoo <dnxprbs@gmail.com>
2022-09-08 12:27:45 +0900

    S3T-433-Feat: PlaceDto 요청 클래스 추가

--- src/main/java/com/heylocal/traveler/dto/PlaceDto.java
+++ src/main/java/com/heylocal/traveler/dto/PlaceDto.java
@@ -1,11 +1,42 @@
 package com.heylocal.traveler.dto;
 
 import com.heylocal.traveler.domain.place.Place;
+import com.heylocal.traveler.domain.place.PlaceCategory;
 import com.heylocal.traveler.domain.plan.list.PlaceItem;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 public class PlaceDto {
+
+	@Getter
+	@Setter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Builder
+	@Schema(description = "장소 상세 정보 요청 DTO")
+	public static class PlaceRequest {
+		@Positive
+		private long id;
+		@NotNull
+		private PlaceCategory category;
+		@NotEmpty
+		private String name;
+		@NotEmpty
+		private String roadAddress;
+		@NotEmpty
+		private String address;
+		@Positive
+		private double lat;
+		@Positive
+		private double lng;
+		@NotEmpty
+		private String kakaoLink;
+	}
+
 	@Getter
 	@Setter
 	@NoArgsConstructor
