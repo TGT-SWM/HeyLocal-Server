27551ee652db28fde8fc3bb0eff4650389252c18
Woody <gintooooonic@gmail.com>
2022-09-17 05:12:19 +0900

    S3T-431-Fix: 사용하지 않은 메서드 삭제
    updateDaySchedule() 메서드를 삭제합니다.

--- src/main/java/com/heylocal/traveler/domain/plan/Plan.java
+++ src/main/java/com/heylocal/traveler/domain/plan/Plan.java
@@ -39,8 +39,4 @@ public class Plan extends BaseTimeEntity {
   @Builder.Default
   @OneToMany(mappedBy = "plan", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
   private List<DaySchedule> dayScheduleList = new ArrayList<>();
-
-  public void updateDayScheduleList(List<DaySchedule> dayScheduleList) {
-    this.dayScheduleList = dayScheduleList;
-  }
 }
