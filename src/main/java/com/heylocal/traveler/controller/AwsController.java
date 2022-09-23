47b6816c333a1eaefe7ae04f85a6f1eb36bb9e32
TaegyunWoo <dnxprbs@gmail.com>
2022-09-23 16:29:33 +0900

    S3T-500-Fix: SNS 구독 확인 로직 추가

--- src/main/java/com/heylocal/traveler/controller/AwsController.java
+++ src/main/java/com/heylocal/traveler/controller/AwsController.java
@@ -5,10 +5,12 @@ import com.heylocal.traveler.exception.NotFoundException;
 import com.heylocal.traveler.service.OpinionImgContentService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.heylocal.traveler.dto.aws.AwsSnsDto.AwsSnsRequest;
 
+@Slf4j
 @Tag(name = "Aws")
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +19,7 @@ public class AwsController implements AwsApi {
 
   @Override
   public void postSavedOpinionImgMessage(AwsSnsRequest request) throws NotFoundException {
+    log.info("SNS 구독 URL: {}", request.getSubscribeURL());
     opinionImgContentService.saveOpinionImageContent(request.getObject());
   }
 }
