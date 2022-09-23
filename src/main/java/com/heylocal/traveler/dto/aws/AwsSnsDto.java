64e1870ae02f6182978357d6490796833b7dd342
TaegyunWoo <dnxprbs@gmail.com>
2022-09-23 16:29:11 +0900

    S3T-500-Fix: SNS 구독 확인 로직 추가

--- src/main/java/com/heylocal/traveler/dto/aws/AwsSnsDto.java
+++ src/main/java/com/heylocal/traveler/dto/aws/AwsSnsDto.java
@@ -13,5 +13,12 @@ public class AwsSnsDto {
   public static class AwsSnsRequest {
     private S3ObjectDto object;
     private LocalDateTime eventTime;
+
+    private String type;
+    private String messageId;
+    private String token;
+    private String subscribeURL;
+    private String signature;
+    private String signingCertURL;
   }
 }
