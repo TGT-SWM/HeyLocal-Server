180401ab771997a55415dc694236caedf65d4c16
TaegyunWoo <dnxprbs@gmail.com>
2022-09-24 21:26:41 +0900

    S3T-500-Fix: 답변 이미지 엔티티의 Object Key 가 없는 경우에만 저장하도록 수정

--- src/main/java/com/heylocal/traveler/repository/OpinionImageContentRepository.java
+++ src/main/java/com/heylocal/traveler/repository/OpinionImageContentRepository.java
@@ -4,7 +4,9 @@ import com.heylocal.traveler.domain.travelon.opinion.OpinionImageContent;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import java.util.Optional;
 
 @Repository
 public class OpinionImageContentRepository {
@@ -13,5 +15,21 @@ public class OpinionImageContentRepository {
 
   public void save(OpinionImageContent entity) {
     em.persist(entity);
+  }
+
+  public Optional<OpinionImageContent> findByObjectKeyName(String objectKeyName) {
+    String jpql = "select o from OpinionImageContent o" +
+        " where o.objectKeyName = :objectKeyName";
+    OpinionImageContent entity;
+
+    try {
+      entity = em.createQuery(jpql, OpinionImageContent.class)
+          .setParameter("objectKeyName", objectKeyName)
+          .getSingleResult();
+    } catch (NoResultException e) {
+      return Optional.empty();
+    }
+
+    return Optional.ofNullable(entity);
   }
 }
--- src/main/java/com/heylocal/traveler/service/OpinionImgContentService.java
+++ src/main/java/com/heylocal/traveler/service/OpinionImgContentService.java
@@ -3,7 +3,9 @@ package com.heylocal.traveler.service;
 import com.heylocal.traveler.domain.travelon.opinion.Opinion;
 import com.heylocal.traveler.domain.travelon.opinion.OpinionImageContent;
 import com.heylocal.traveler.dto.aws.S3ObjectDto;
+import com.heylocal.traveler.exception.BadRequestException;
 import com.heylocal.traveler.exception.NotFoundException;
+import com.heylocal.traveler.exception.code.BadRequestCode;
 import com.heylocal.traveler.exception.code.NotFoundCode;
 import com.heylocal.traveler.mapper.OpinionImageContentMapper;
 import com.heylocal.traveler.repository.OpinionImageContentRepository;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Map;
+import java.util.Optional;
 
 import static com.heylocal.traveler.domain.travelon.opinion.OpinionImageContent.ImageContentType;
 import static com.heylocal.traveler.util.aws.S3ObjectNameFormatter.ObjectNameProperty;
@@ -45,6 +48,7 @@ public class OpinionImgContentService {
     Opinion targetOpinion;
 
     objectKeyName = s3ObjectDto.getKey();
+
     objectNamePropertyMap = s3ObjectNameFormatter.parseObjectNameOfOpinionImg(objectKeyName);
     targetImageType = Enum.valueOf(ImageContentType.class, objectNamePropertyMap.get(ObjectNameProperty.IMG_TYPE));
     targetTravelOnId = Long.parseLong(objectNamePropertyMap.get(ObjectNameProperty.TRAVEL_ON_ID));
@@ -54,6 +58,11 @@ public class OpinionImgContentService {
     );
 
     OpinionImageContent target = OpinionImageContentMapper.INSTANCE.toEntity(objectKeyName, targetImageType, targetOpinion);
-    opinionImageContentRepository.save(target);
+
+    //이미 존재하는 Object Key 가 아닌 경우
+    Optional<OpinionImageContent> existedImgContent = opinionImageContentRepository.findByObjectKeyName(objectKeyName);
+    if ( !existedImgContent.isPresent() ) {
+      opinionImageContentRepository.save(target); //저장
+    }
   }
 }
