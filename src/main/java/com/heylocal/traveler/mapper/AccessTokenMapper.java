f8dd676af6126ce1bebabbbfb5418b52d4abdd96
TaegyunWoo <dnxprbs@gmail.com>
2022-10-19 20:53:02 +0900

    S3T-338-Refactor: 토큰 Redis 엔티티 Mapper 의 기존 하드코딩 스니펫 수정

--- src/main/java/com/heylocal/traveler/mapper/AccessTokenMapper.java
+++ src/main/java/com/heylocal/traveler/mapper/AccessTokenMapper.java
@@ -2,7 +2,7 @@
  * packageName    : com.heylocal.traveler.mapper
  * fileName       : AccessTokenMapper
  * author         : 우태균
- * date           : 2022/10/18
+ * date           : 2022/10/19
  * description    : AccessToken 레디스 엔티티 관련 Mapper
  */
 
@@ -13,6 +13,7 @@ import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+import java.lang.reflect.Field;
 import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -21,37 +22,61 @@ import java.util.concurrent.ConcurrentHashMap;
 public interface AccessTokenMapper {
   AccessTokenMapper INSTANCE = Mappers.getMapper(AccessTokenMapper.class);
 
-  default Map<String, String> entityToMap(AccessToken accessToken) {
+  default Map<String, String> entityToMap(AccessToken accessToken) throws IllegalAccessException {
     Map<String, String> result = new ConcurrentHashMap<>();
+    Field[] declaredFields = AccessToken.class.getDeclaredFields();
 
-    String userId = String.valueOf(accessToken.getUserId());
-    String tokenValue = accessToken.getTokenValue();
-    String timeoutSec = String.valueOf(accessToken.getTimeoutSec());
-    String refreshTokenId = String.valueOf(accessToken.getRefreshTokenId());
-    String createdDate = accessToken.getCreatedDate().toString();
+    //RefreshToken 에 선언한 필드마다 반복
+    for (Field field : declaredFields) {
+      Class<?> fieldType = field.getType();
+      String fieldName = field.getName();
+      String fieldValue = "";
+      field.setAccessible(true); //private 필드 접근 허용
 
-    result.put("userId", userId);
-    result.put("tokenValue", tokenValue);
-    result.put("timeoutSec", timeoutSec);
-    result.put("refreshTokenId", refreshTokenId);
-    result.put("createdDate", createdDate);
+      //String 형으로 변환
+      if (fieldType.equals(Long.class)) { //Long 타입인 경우
+        fieldValue = String.valueOf(field.get(accessToken));
+
+      } else if (fieldType.equals(LocalDateTime.class)) { //LocalDateTime 타입인 경우
+        LocalDateTime tmp = (LocalDateTime) field.get(accessToken);
+        fieldValue = tmp.toString();
+
+      } else if (fieldType.equals(String.class)) { //String 타입인 경우
+        fieldValue = (String) field.get(accessToken);
+      }
+
+      result.put(fieldName, fieldValue);
+    }
 
     return result;
   }
 
-  default AccessToken mapToEntity(Map<String, String> entries) {
-    long userId = Long.parseLong(entries.get("userId"));
-    String tokenValue = entries.get("tokenValue");
-    long timeoutSec = Long.parseLong(entries.get("timeoutSec"));
-    long refreshTokenId = Long.parseLong(entries.get("refreshTokenId"));
-    LocalDateTime createdDate = LocalDateTime.parse(entries.get("createdDate"));
+  default AccessToken mapToEntity(Map<String, String> entries) throws IllegalAccessException {
+    AccessToken result = new AccessToken();
+    Field[] declaredFields = AccessToken.class.getDeclaredFields();
 
-    return AccessToken.builder()
-        .userId(userId)
-        .tokenValue(tokenValue)
-        .timeoutSec(timeoutSec)
-        .refreshTokenId(refreshTokenId)
-        .createdDate(createdDate)
-        .build();
+    //RefreshToken 에 선언한 필드마다 반복
+    for (Field field : declaredFields) {
+      Class<?> fieldType = field.getType();
+      String fieldName = field.getName();
+      field.setAccessible(true); //private 필드 접근 허용
+
+      //String 형으로 변환
+      if (fieldType.equals(Long.class)) { //Long 타입인 경우
+        Long fieldValue = Long.parseLong(entries.get(fieldName));
+        field.set(result, fieldValue);
+
+      } else if (fieldType.equals(LocalDateTime.class)) { //LocalDateTime 타입인 경우
+        LocalDateTime fieldValue = LocalDateTime.parse(entries.get(fieldName));
+        field.set(result, fieldValue);
+
+      } else if (fieldType.equals(String.class)) { //String 타입인 경우
+        String fieldValue = entries.get(fieldName);
+        field.set(result, fieldValue);
+      }
+
+    }
+
+    return result;
   }
 }
--- src/main/java/com/heylocal/traveler/mapper/RefreshTokenMapper.java
+++ src/main/java/com/heylocal/traveler/mapper/RefreshTokenMapper.java
@@ -2,7 +2,7 @@
  * packageName    : com.heylocal.traveler.mapper
  * fileName       : RefreshTokenMapper
  * author         : 우태균
- * date           : 2022/10/18
+ * date           : 2022/10/19
  * description    : RefreshToken 레디스 엔티티 관련 Mapper
  */
 
@@ -13,6 +13,7 @@ import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+import java.lang.reflect.Field;
 import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -21,37 +22,61 @@ import java.util.concurrent.ConcurrentHashMap;
 public interface RefreshTokenMapper {
   RefreshTokenMapper INSTANCE = Mappers.getMapper(RefreshTokenMapper.class);
 
-  default Map<String, String> entityToMap(RefreshToken refreshToken) {
+  default Map<String, String> entityToMap(RefreshToken refreshToken) throws IllegalAccessException {
     Map<String, String> result = new ConcurrentHashMap<>();
+    Field[] declaredFields = RefreshToken.class.getDeclaredFields();
 
-    String userId = String.valueOf(refreshToken.getUserId());
-    String tokenValue = refreshToken.getTokenValue();
-    String timeoutSec = String.valueOf(refreshToken.getTimeoutSec());
-    String accessTokenId = String.valueOf(refreshToken.getAccessTokenId());
-    String createdDate = refreshToken.getCreatedDate().toString();
+    //RefreshToken 에 선언한 필드마다 반복
+    for (Field field : declaredFields) {
+      Class<?> fieldType = field.getType();
+      String fieldName = field.getName();
+      String fieldValue = "";
+      field.setAccessible(true); //private 필드 접근 허용
 
-    result.put("userId", userId);
-    result.put("tokenValue", tokenValue);
-    result.put("timeoutSec", timeoutSec);
-    result.put("accessTokenId", accessTokenId);
-    result.put("createdDate", createdDate);
+      //String 형으로 변환
+      if (fieldType.equals(Long.class)) { //Long 타입인 경우
+        fieldValue = String.valueOf(field.get(refreshToken));
+
+      } else if (fieldType.equals(LocalDateTime.class)) { //LocalDateTime 타입인 경우
+        LocalDateTime tmp = (LocalDateTime) field.get(refreshToken);
+        fieldValue = tmp.toString();
+
+      } else if (fieldType.equals(String.class)) { //String 타입인 경우
+        fieldValue = (String) field.get(refreshToken);
+      }
+
+      result.put(fieldName, fieldValue);
+    }
 
     return result;
   }
 
-  default RefreshToken mapToEntity(Map<String, String> entries) {
-    long userId = Long.parseLong(entries.get("userId"));
-    String tokenValue = entries.get("tokenValue");
-    long timeoutSec = Long.parseLong(entries.get("timeoutSec"));
-    long accessTokenId = Long.parseLong(entries.get("accessTokenId"));
-    LocalDateTime createdDate = LocalDateTime.parse(entries.get("createdDate"));
+  default RefreshToken mapToEntity(Map<String, String> entries) throws IllegalAccessException {
+    RefreshToken result = new RefreshToken();
+    Field[] declaredFields = RefreshToken.class.getDeclaredFields();
 
-    return RefreshToken.builder()
-        .userId(userId)
-        .tokenValue(tokenValue)
-        .timeoutSec(timeoutSec)
-        .accessTokenId(accessTokenId)
-        .createdDate(createdDate)
-        .build();
+    //RefreshToken 에 선언한 필드마다 반복
+    for (Field field : declaredFields) {
+      Class<?> fieldType = field.getType();
+      String fieldName = field.getName();
+      field.setAccessible(true); //private 필드 접근 허용
+
+      //String 형으로 변환
+      if (fieldType.equals(Long.class)) { //Long 타입인 경우
+        Long fieldValue = Long.parseLong(entries.get(fieldName));
+        field.set(result, fieldValue);
+
+      } else if (fieldType.equals(LocalDateTime.class)) { //LocalDateTime 타입인 경우
+        LocalDateTime fieldValue = LocalDateTime.parse(entries.get(fieldName));
+        field.set(result, fieldValue);
+
+      } else if (fieldType.equals(String.class)) { //String 타입인 경우
+        String fieldValue = entries.get(fieldName);
+        field.set(result, fieldValue);
+      }
+
+    }
+
+    return result;
   }
 }
